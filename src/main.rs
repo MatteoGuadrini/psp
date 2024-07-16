4bd493bed1d242c47409a480780d3378305c49e8
Matteo Guadrini <matteo.guadrini@hotmail.it>
2024-07-16 11:41:34 +0200

    chore: add check_tool function

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,14 @@
+use std::{path::Path, process::exit};
+
+// Function for check if tool is installed
+fn check_tool(tool: &str) {
+    if !Path::new(tool).exists() {
+        eprintln!("error: the tool {} not installed", tool);
+        exit(1);
+    }
+}
+
 fn main() {
     println!("Hello by PSP (Python Scaffolding Projects!)");
-    println!("This project is a WIP! Be careful!")
+    println!("This project is a WIP! Be careful!");
 }
