2328478292e7f79da3476fc8faee0ac7d174ba0e
Matteo Guadrini <matteo.guadrini@hotmail.it>
2024-07-26 09:56:50 +0200

    chore: add make_file function

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
-use std::{fs::create_dir_all, path::Path, process::exit};
+use std::{
+    fs::{create_dir_all, File},
+    io::Write,
+    path::Path,
+    process::exit,
+};
 
 // Function for check if tool is installed
 fn check_tool(tool: &str) {
@@ -17,6 +22,19 @@ fn make_dirs(dir: &str) {
             exit(2);
         }
         Ok(_) => (),
+    }
+}
+
+// Function for creating file with contents
+fn make_file(file: &str, content: String) -> std::io::Result<()> {
+    let mut file = File::create(file)?;
+    let result = file.write_all(&content.as_bytes());
+    match result {
+        Err(e) => {
+            eprintln!("error: {}", e);
+            exit(3);
+        }
+        Ok(_) => Ok(()),
     }
 }
 
